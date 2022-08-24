b6ef32368f6310d4c8514fbe9bd05e6146d5712c
samuel <sbacon803@gmail.com>
2022-08-24 12:01:26 -0600

    add index action

---
+++ app/controllers/places_controller.rb
@@ -0,0 +1,7 @@
+class PlacesController < ApplicationController
+  def index
+    places = Place.all
+
+    render json: places.as_json
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+place = Place.new(name: "Wrigley Field", address: "1415 McKinley Ave, Chicago, Il")
+# place.save
