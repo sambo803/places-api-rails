839a42b067b5cbb055fa2ee3bfc3bd21d3d90748
samuel <sbacon803@gmail.com>
2022-08-27 16:43:26 -0600

    add post create action

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -3,4 +3,12 @@ class PlacesController < ApplicationController
     places = Place.all
     render json: places.as_json
   end
+
+  def create
+    place = Place.new(name: params[:name],
+                      address: params[:address],
+                      image_url: params[:image_url])
+    place.save
+    render json: place.as_json
+  end
 end
